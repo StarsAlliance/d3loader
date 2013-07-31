16cebbde7f792f0267922e01c894d270f27c4053
DragonZX <dragonzx@yandex.ru>
2013-07-31 12:22:28 +0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.IO;
+
+namespace D3Loader
+{
+    class Start
+    {
+        static void Main(string[] args)
+        {
+            Process d3 = new Process();
+            if (File.Exists("Diablo III.exe"))
+            {
+            d3.StartInfo.FileName = "Diablo III.exe";
+            d3.StartInfo.Arguments = "-launch";
+            d3.StartInfo.CreateNoWindow = true;
+            d3.StartInfo.UseShellExecute = false;
+            System.Console.WriteLine("Starting the game");
+            d3.Start();
+            }
+            else
+            { 
+            System.Console.WriteLine("Diablo III.exe wasn't found!"); //error message
+            System.Threading.Thread.Sleep(2000); //Pause before closing
+            }
+        }
+    }
+}
