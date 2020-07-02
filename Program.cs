a29353089bd8ecc1bf2c16a6945a18a0cf95808a
DragonZX <DragonZX@yandex.ru>
2020-07-02 18:16:05 +0300

    Cleanup + Refactoring

--- Program.cs
+++ Program.cs
@@ -2,27 +2,29 @@
 using System.Text;
 using System.Diagnostics;
 using System.IO;
+using D3Loader.Properties;
 
 namespace D3Loader
 {
-    class Start
+    internal static class Start
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Process d3 = new Process();
-            if (File.Exists("Diablo III.exe"))
+            var d3 = new Process();
+            const string exec = "Diablo III.exe";
+            if (File.Exists(exec))
             {
-            d3.StartInfo.FileName = "Diablo III.exe";
-            d3.StartInfo.Arguments = "-launch";
-            d3.StartInfo.CreateNoWindow = true;
-            d3.StartInfo.UseShellExecute = false;
-            System.Console.WriteLine("Starting the game");
-            d3.Start();
+                d3.StartInfo.FileName = exec;
+                d3.StartInfo.Arguments = "-launch";
+                d3.StartInfo.CreateNoWindow = true;
+                d3.StartInfo.UseShellExecute = false;
+                System.Console.WriteLine("Starting the game");
+                d3.Start();
             }
             else
             { 
-            System.Console.WriteLine("Diablo III.exe wasn't found!"); //error message
-            System.Threading.Thread.Sleep(2000); //Pause before closing
+                System.Console.WriteLine(exec, " wasn't found"); //error message
+                System.Threading.Thread.Sleep(2000); //Pause before closing
             }
         }
     }
